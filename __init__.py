43b58790c951a60ccdf95ca1b19d9490ba543183
bb6 <73919236+bgofish@users.noreply.github.com>
2026-04-05 13:32:21 +1200

    DRAFT 0.1.0

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2025
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import lichtfeld as lf
+from .panels.bg360_panel import BG360Panel
+
+_classes = [BG360Panel]
+
+
+def on_load():
+    for cls in _classes:
+        lf.register_class(cls)
+    lf.log.info("BG360 loaded")
+
+
+def on_unload():
+    for cls in reversed(_classes):
+        lf.unregister_class(cls)
+    lf.log.info("BG360 unloaded")
